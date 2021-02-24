b9cb19579830f9c0ff6e16751c7e191b1fdf1280
Kevin Ramirez <kramirez234@west-mec.org>
2021-02-23 18:25:40 -0700

    Check point in creating the models
    Almost done creating the models.

--- models/people.js
+++ models/people.js
@@ -11,38 +11,12 @@ var PeopleSchema = new Schema(
         family_name: { type: String, required: true, maxlength: 100 },
         date_of_birth: { type: Date },
         date_of_death: { type: Date },
+        height: {type: String, required: true, maxlength: 3},
+        weight: {type: String, required: true, maxlength: 3},
+        
     }
 );
 
-// Virtual for author "full" name.
-PeopleSchema.virtual('name').get(function () {
-    return this.family_name + ', ' + this.first_name;
-});
-
-// Virtual for this author instance URL.
-PeopleSchema.virtual('url').get(function () {
-    return '/catalog/people/' + this._id;
-});
-
-PeopleSchema.virtual('lifespan').get(function () {
-    var lifetime_string = '';
-    if (this.date_of_birth) {
-        lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
-    }
-    lifetime_string += ' - ';
-    if (this.date_of_death) {
-        lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
-    }
-    return lifetime_string;
-});
-
-PeopleSchema.virtual('date_of_birth_yyyy_mm_dd').get(function () {
-    return DateTime.fromJSDate(this.date_of_birth).toISODate(); //format 'YYYY-MM-DD'
-});
-
-PeopleSchema.virtual('date_of_death_yyyy_mm_dd').get(function () {
-    return DateTime.fromJSDate(this.date_of_death).toISODate(); //format 'YYYY-MM-DD'
-});
 
 // Export model.
 module.exports = mongoose.model('People', PeopleSchema);
--- models/vehicle.js
+++ models/vehicle.js
@@ -4,9 +4,24 @@ const { DateTime } = require("luxon");
 var Schema = mongoose.Schema;
 
 var VehicleSchema = new Schema({
+    vehicle_make:{},
+    vehicle_model:{},
+    vehicle_plate:{},
+    vehicle_year:{},
+    vehicle_miles:{},
+    vehicle_title:{},
+    vehicle_color:{},
+    vehcile_vin:{},
+    vehicle_details:{},
+    vehicle_registration:{},
+    vehicle_body:{}
+
 
 });
 
 VehicleSchema.virtual('url').get(function(){
-    return '/form/incident/' + this._id;
+    return '/form/vehicle/' + this._id;
 })
+
+// Export model.
+module.exports = mongoose.model('Vehicle', VehicleSchema);
