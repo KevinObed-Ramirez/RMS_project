2b8ee7b54464416a363c2385e0201e2a0a7d9752
Kevin Ramirez <kramirez234@west-mec.org>
2021-03-03 18:24:45 -0700

    Trying to fix controllers

--- controllers/peopleController.js
+++ controllers/peopleController.js
@@ -1,5 +1,5 @@
 var People = require('../models/people');
-// var async = require('async');
+ var async = require('async');
 var Incident = require('../models/incident');
 
  const { body,validationResult } = require('express-validator')
