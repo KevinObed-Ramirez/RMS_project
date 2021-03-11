841fa55ff2b158e3b92a39ecd04e6e61794e02df
Kevin Ramirez <kramirez234@west-mec.org>
2021-03-10 18:16:04 -0700

    Styles fix, vehicle form, index pug, and layout pug

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(compression()); //Compress all routes
 
 app.use(express.static(path.join(__dirname, 'public')));
 
